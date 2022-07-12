146aaa4e78c2f607640827a746a98a7fe53904e6
Chandan Maharana <85704460+lifeforcoding@users.noreply.github.com>
2022-07-12 18:10:25 +0530

    Create QuickSort.java

---
+++ QuickSort.java
@@ -0,0 +1,47 @@
+public static void main(String[] args) {
+        int[] a = new int[]{7,4,9,1,8,2,3,5};
+        quickSort(a, 0, a.length - 1);
+        for (int e : a)
+            System.out.print(e + " ");
+    }
+    //Function for quick sort Algo
+    public static void quickSort(int[] arr, int s, int e) {
+        //Base case
+        if (s >= e)
+            return;
+        //Make the partition and place the pivot in their correct place
+        int pivot = partition(arr, s, e);
+        //Sort the lest side of the pivot
+        quickSort(arr, s, pivot - 1);
+        //Sort the right side of the pivot
+        quickSort(arr, pivot + 1, e);
+    }
+
+    //Partition Function
+    public static int partition(int[] arr, int s, int e) {
+        int count = s; // To store the pivot index
+        for (int i = s + 1; i <= e; i++) {
+            if (arr[i] < arr[s])
+                count++;
+        }
+        swap(arr, s, count);
+        int k = count + 1; // Starting index after pivot point
+        int m = s; //Starting index before the pivot point
+        //Swapping element greater than the pivot to the left side
+        while (m < count && k <= e) {
+            if (arr[m] > arr[count] && arr[k] < arr[count]) {
+                swap(arr, m, k);
+                m++;
+            }
+            k++;
+        }
+
+        return count;
+    }
+
+    //Swapping function
+    public static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
