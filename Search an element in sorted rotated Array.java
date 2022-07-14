89384485bb87a52bc8fddae962a2d27fecb60d44
Chandan Maharana <85704460+lifeforcoding@users.noreply.github.com>
2022-07-15 01:46:55 +0530

    Create Search an element in sorted rotated Array.java

---
+++ Search an element in sorted rotated Array.java
@@ -0,0 +1,24 @@
+//Search an element in a sorted rotated array
+    public static int searchInRotated(int[] a, int key) {
+        int l = 0;
+        int r = a.length - 1;
+        while(l <= r) {
+            int mid = l + (r - l)/2;
+            System.out.println(mid);
+            if(a[mid] == key) return mid;
+            if(a[l] <= a[mid]) { // left part sorted
+                if(key >= a[l] && key < a[mid]) { // left side present
+                    r = mid-1;
+                } else { // right side present
+                    l = mid+1;
+                }
+            } else { // right part sorted
+                if(key > a[mid] && key <= a[r]) { // right side present
+                    l = mid+1;
+                } else { // left side present
+                    r = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
