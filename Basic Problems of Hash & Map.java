ceb66688c67871fed04e6dbacdfb97d161030d2a
Chandan Maharana <85704460+lifeforcoding@users.noreply.github.com>
2022-07-28 13:26:55 +0530

    Update Basic Problems of Hash & Map.java

--- Basic Problems of Hash & Map.java
+++ Basic Problems of Hash & Map.java
@@ -1,3 +1,50 @@
+//Basic Set and Map Problems
+    //Count Distinct element in an array
+    public static int countDistinct(int[] a) {
+        Set<Integer> dist = new HashSet<>();
+        for (int e : a) {
+            dist.add(e);
+        }
+        return dist.size();
+    }
+
+    //Frequency of element in an array
+    public static void frequencyOfElement(int[] a) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int e : a) {
+            if (!freq.containsKey(e))
+                freq.put(e, 1);
+            else {
+                int value= freq.get(e);
+                value++;
+                freq.put(e, value);
+            }
+        }
+        System.out.println(freq);
+    }
+
+    //Pair with given sum in an unsorted array
+
+
+    public static pair givenSumPair(int[] a, int sum) {
+        Set<Integer> s1 = new HashSet<>();
+        for (int e : a) {
+            int compSum = sum - e;
+            if (s1.contains(compSum))
+                return new pair(e, compSum);
+            s1.add(e);
+        }
+        return null;
+    }
+}
+class pair {
+    int a, b;
+
+    public pair(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+}
 //Check if the array has sub array of 0 sum
     public static boolean subArrayZero(int[] a) {
         Set<Integer> set = new HashSet<>();
